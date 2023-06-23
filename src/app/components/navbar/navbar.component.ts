4b071984d24be67476ab8e3cd449e37fa831c518
Jonas Souza <jonasmzsouza@gmail.com>
2023-06-23 03:10:45 -0300

    chore: responsive navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  collapsed: string = 'navbar__collapse';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  collapse() {
+    if (this.collapsed == 'navbar__collapse') {
+      this.collapsed = 'navbar__collapse show';
+    } else {
+      this.collapsed = 'navbar__collapse';
+    }
   }
-
 }
